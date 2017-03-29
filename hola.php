a8df852028799d019e963a3fba4ca7bdce285e37
Eloy Hernández <eloyhz@gmail.com>
2017-03-29 12:58:38 -0600

    Add files via upload

---
+++ hola.php
@@ -0,0 +1,9 @@
+<?php
+
+// este es un script de php
+
+# este es un comentario estilo Perl
+
+echo '<h1>Hola PHP</h1>';
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php
+     # variables: TODAS las variables se declaran con $
+     $num1 = 10;
+     $num2 = 20;
+     $suma = $num1 + $num2;
+   // echo 'La suma es $suma'; 
+   // echo "La suma es $suma"; 
+    
+    # La concatenación de cadenas se hace con el .
+    echo 'La suma es ' . $suma;
+
+    # ciclos, condicionales, switch, etc son lo mismo que en JS 
+
+    ?>
+    </h1>
+</body>
+</html>
